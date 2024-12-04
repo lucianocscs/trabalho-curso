5bd2d6e7bf0beffd4c4112b00287e63e457d1bdc
luciano costa <lucianocscs2022@gmail.com>
2024-12-04 12:28:50 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,56 @@
+let dados =  [
+    
+    {
+      titulo : "Energia Hidráulica",
+      descricao : "Uma das mais antigas: A energia da água em movimento, utilizada para moer grãos e bombear água desde a antiguidade. Evolução: Com o desenvolvimento de turbinas e barragens, a energia hidrelétrica se tornou uma fonte de energia elétrica em larga escala.",
+      link:"https://pt.wikipedia.org/wiki/Energia_hidr%C3%A1ulica#:~:text=Ela%20consiste%20basicamente%20em%20uma,%C3%A1gua%20durante%20o%20ano%20inteiro.",
+      tags : "Renovàveis"
+    },
+  
+    {
+    titulo: "Energia Biomassa",
+  
+        descricao : "Energia obtida a partir da conversão de biomassa (matéria orgânica) em energia útil.",
+        link: "https://pt.wikipedia.org/wiki/Biomassa",
+        tags : "Biomassa",
+    },
+    {
+      titulo : "Energia Eólica",
+  
+      descricao : "A energia eólica aproveita a força do vento para gerar eletricidade. Evolução: As turbinas eólicas se tornaram mais eficientes e os parques eólicos são cada vez mais comuns.",
+      link: "https://youtu.be/GifPlJ0bn7o?t=105",
+      tags : "Eólica"
+    },
+
+    {
+      titulo :  "Energia Solar",
+      descricao : "Converte a radiação solar em energia elétrica através de painéis fotovoltaicos ou em energia térmica através de coletores solares.",
+      link : "https://pt.wikipedia.org/wiki/Energia_solar",
+      tags : "Solar"
+    },
+    {
+        titulo :  "Energia Geotérmica",
+  
+        descricao : "Aproveita o calor interno da Terra para gerar eletricidade e aquecimento.",
+        link : "https://pt.wikipedia.org/wiki/Energia_geotérmica",
+        tags : "geotermica"
+      },
+      {
+        titulo :  "Energia do Hidrogênio",
+        descricao : "Utiliza o hidrogênio como combustível para gerar eletricidade em células de combustível.",
+        link : "https://pt.wikipedia.org/wiki/Hidrogênio_como_combustível",
+        tags : "hidrogenio",
+        
+      },
+      {
+      titulo : "Energia das Ondas e Marés",
+      descricao : "Converte a energia cinética das ondas e marés em eletricidade.",
+      link : "https://youtu.be/KWeeVygrfHo",
+      tags : " mares"
+      },
+  
+  ];
+  
+  
+  
+      
